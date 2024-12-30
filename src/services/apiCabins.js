773da40c4c8f2d1380dcfe3921102b78b81aaffe
oxanadev <shana_bj@hotmail.com>
2024-12-30 14:23:08 +0500

    Duplicated cabins, fetched & updated app settings

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -40,6 +40,8 @@ export async function createEditCabin(newCabin, id) {
   }
 
   // Upload an image
+  if (hasImagePath) return data;
+
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
     .upload(imageName, newCabin.image);
